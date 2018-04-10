0633412533004a598735905430440c755d00eceb
Mohsin Reshi <mohsin_reshi@yahoo.com>
2018-04-10 17:37:53 +0530

    remove prelim DOM functions

--- script.js
+++ script.js
@@ -20,30 +20,3 @@ function operate(a, operator, b) {
 
 const buttons = document.querySelectorAll('.button');
 const button = document.querySelector('.button');
-const operands = document.querySelectorAll('.operand');
-const operando = document.querySelector('.operand');
-
-/*function returnButtonValue(){
-  buttons.forEach(button => button.addEventListener('click', function(){
-    return button.value;
-}))}*/
-
-function operand() {
-  operands.forEach(operando => operando.onclick = function() {
-    let temp = operando.value;
-    return temp.toString();
-  })
-}
-
-function buttonValue(){
-  buttons.forEach(button => button.onclick = function() {
-  return button.value;
-})}
-
-const equalButton = document.querySelector('.equal');
-let a = buttonValue()
-  , operator = operand()
-  , b = buttonValue();
-equalButton.onclick = function() {
-  operate(a, operator, b);
-}
